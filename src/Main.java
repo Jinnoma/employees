9aa3130f01426087450632f01db26c034904df7d
Jinnoma <przemyslaw.guzek@gmail.com>
2020-11-24 20:18:50 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
-    public static void main(String args[]){
-        OfficeEmployee employee = new OfficeEmployee("Przemek", "Guzek", 11, "doświadczony", 123, 70);
-        System.out.println();
+    public static void main(String args[]) {
 
+        Object emp;
+        Address address = new Address("Augusta", 12, 13, "Gdynia");
+        OfficeEmployee employee = new OfficeEmployee("Przemek", "Guzek", 11, 1, 123, 70, address);
+        Registry registry = new Registry();
+        registry.addEmployee(employee);
+        emp = registry.getEmployee(0);
+        emp.getFirstName();
 
-
-
+    }
 }

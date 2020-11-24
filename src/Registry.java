e014fe03c2549d87c2faf2531172cf1c870512dd
Jinnoma <przemyslaw.guzek@gmail.com>
2020-11-24 19:25:01 +0100

    Update Registry.java

--- src/Registry.java
+++ src/Registry.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Registry {
+    private List<Object> employees = new ArrayList<Object>();
+
+    public Registry(){
+
+    }
+
+    public void addOfficeEmployee(OfficeEmployee officeEmployee){
+//        OfficeEmployee officeEmployee = new OfficeEmployee("przemek", "Guzek",30, 1, 123, 60, 234);
+        employees.add(officeEmployee);
+    }
 
 }
